86d9a26de1c738c41e7c2ff4a8fa096fbf55c52a
Katherine Frain <katherfrain@gmail.com>
2020-08-25 10:28:50 -0400

    dictionary practice from last night

--- dictionary-practice1.py
+++ dictionary-practice1.py
@@ -6,6 +6,17 @@ phonebook_dict = {
 print(phonebook_dict["Elizabeth"])
 phonebook_dict["Kareem"] = "455-617-9124"
 phonebook_dict["Alice"] = "Number not found"
+
+print(phonebook_dict["Alice"])
+# del phonebook_dict["Alice"]
+retained_alice = phonebook_dict.pop("Alice")
+try:
+    print(phonebook_dict["Alice"])
+except KeyError:
+    print("Alice isn't a keyword anymore")
+
+print(retained_alice)
+
 phonebook_dict["Bob"] = "968-489-2154"
 print("*" * 20)
 
